3740272b91290ca33a6746d9a0596d8a61f7f042
Omar Salem <omarsalem0721@gmail.com>
2025-07-19 11:54:19 +0300

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    suppressHydrationWarning: true, // Use with caution
-  },
   images: {
     remotePatterns: [
       {
